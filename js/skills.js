199d5a1bd441e1fe6842cd9e9716f5b20fe0e8b4
Sneh Sagar Rajput <sneh.sagar98@gmail.com>
2020-11-04 01:56:54 +0530

    fixed text overflow

--- js/skills.js
+++ js/skills.js
@@ -2,16 +2,16 @@ import alpha from "./master.js";
 
 
 const skillsList = [
-      'I know a few Programming Languages <i class="fas fa-laptop-code"></i><br>C &nbspC++ &nbspPython',
+      'I know a few Programming Languages <i class="fas fa-laptop-code"></i>&nbsp&nbsp &nbsp <br> C &nbspC++ &nbspPython<br> &nbsp',
       'I develop frontend applications using<br><i class="fab fa-html5"></i> &nbsp<i class="fab fa-css3-alt"></i>' +
       ' &nbsp<i class="fab fa-js"></i> &nbsp<i class="fab fa-react"></i> ',
-      'I use these frameworks often <i class="fas fa-layer-group"></i><br>Tensorflow &nbspReact-Native',
-      'I love to play with these tools <i class="fas fa-terminal"></i><br>' +
+      'I use these frameworks often <i class="fas fa-layer-group"></i>&nbsp<br>Tensorflow &nbspReact-Native',
+      'I love to play with these tools <i class="fas fa-terminal"></i>&nbsp<br>' +
       'Github &nbspVS‑Code &nbspGoogle‑Colab',
-      'I like to continuously explore about <i class="fab fa-wpexplorer"></i><br>' +
+      'I like to continuously explore about <i class="fab fa-wpexplorer"></i>&nbsp<br>' +
       'Machine Learning &nbspDeep Learning &nbsp',
       '<i class="fas fa-cookie-bite">Eat</i>&nbsp <i class="fas fa-code">Code</i>&nbsp <i class="fas fa-bed">' +
-      'Sleep</i><br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <i class="fas fa-recycle">Repeat</i>',
+      'Sleep</i>&nbsp<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <i class="fas fa-recycle">Repeat</i>',
 ];
 
 
