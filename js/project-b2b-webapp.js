12b7b2d3c4dd32c8aa81f0bfc6e5fff1dcd2e359
Sneh Sagar Rajput <sneh.sagar98@gmail.com>
2020-12-18 00:16:51 +0530

    added new project and skills

---
+++ js/project-b2b-webapp.js
@@ -0,0 +1,17 @@
+import alpha from "./master.js";
+
+
+const projectsDescription = [
+      '<strong>Fintech B2B Order Management Application</strong> <br>' +
+      'An web application made with ReactJS, MySQL-Database and JAVA backend' +
+      ' for order management requirements in Fintech(Financial Technology) B2B(Business to Business) ecosystem.',
+      '• Developed an Web Application with ReactJS and JAVA backend.',
+      '• Developed order dashboard with different user level login capability.',
+      '• Implemented dynamic search, pagination, approve, reject, add and edit features for order management.',
+      '• Designed responsive and interactive dynamic web pages.',
+      '<a target="_blank" href="https://github.com/snehsagarajput/fintech-order-management-webapp">Check on Github</a> <br> ',
+];
+
+
+
+window.onload = alpha(projectsDescription, 20);
--- js/projects.js
+++ js/projects.js
@@ -4,6 +4,7 @@ import alpha from "./master.js";
 const projectsDescription = [
       'Here are few of my favourite Projects <i class="fas fa-project-diagram"></i> <br>&nbsp',
       '<a target="_blank" href="/project-sudoku-solver-app.html">Sudoku Solver Android App</a>',
+      '<a target="_blank" href="/project-b2b-webapp.html">Fintech B2B Order Management Web App</a><br>',
       '<a target="_blank" href="/project-nst-app.html">Neural Style Transfer Web App</a><br>',
       '<a target="_blank" href="/project-covid.html">Alert System for Social Distancing and Face Mask Detection</a><br> ',
 ];
