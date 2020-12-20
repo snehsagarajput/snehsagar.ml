a1bab4fde23c9a75d48113144db745c5a8d90db5
Sneh Sagar Rajput <sneh.sagar98@gmail.com>
2020-12-20 22:58:12 +0530

    added link for weekly stats

--- js/home.js
+++ js/home.js
@@ -12,6 +12,8 @@ const getCurrentTime = () => {
       if (current >= 22 || current < 5) return 'Have a good night <i class="fas fa-moon"></i>';
 }
 
+const statsLink = 'https://camo.githubusercontent.com/9e1f51206c26725a2141f301d771fa3403836640b8c7e4ca17cc439d7edac0aa/68747470733a2f2f6769746875622d726561646d652d73746174732e76657263656c2e6170702f6170692f77616b6174696d653f757365726e616d653d736e65687361676172616a70757426636f756e745f707269766174653d747275652673686f775f69636f6e733d74727565267468656d653d676f7468616d266c616e67735f636f756e743d35';
+
 const messages = [
       'Hey there 👋',
       'I\'m Sneh Sagar <i class="fas fa-male"></i>',
@@ -19,7 +21,8 @@ const messages = [
       '<br>Checkout my <a href="/education.html">Education</a> <i class="fas fa-user-graduate"></i>',
       'Here are some of my <a href="/skills.html">Skills</a> <i class="fas fa-tools"></i>',
       'I love to write optimized code.<br>' +
-      'Checkout my <a href="/projects.html">Projects</a> <i class="fas fa-laptop-code"></i>',
+      'Checkout my <a href="/projects.html">Projects</a> ' +
+      '& <a href=\"' + statsLink + '\">weekly coding stats</a> <i class="fas fa-laptop-code"></i>',
       'I\'m currently looking for Internships and Full time roles.<br>' +
       ' You can contact me at <a href="mailto:snehsagarajput@gmail.com">snehsagarajput@gmail.com</a> <i class="fas fa-envelope"></i>',
       'Checkout my <a href="/assests/resume.pdf">Resume</a> <i class="fas fa-file"></i>',
